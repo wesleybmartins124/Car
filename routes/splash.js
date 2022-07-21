21df24221e86a5d283146cd2989015075d6aa4de
Wesley <wesley.bm124@gmail.com>
2022-07-21 15:39:51 -0300

    third commit

--- routes/splash.js
+++ routes/splash.js
@@ -6,16 +6,31 @@ import LottieView from  "lottie-react-native";
 export default function Splash(){
   return(
     <View style={styles.container}>
-      <LottieView source={require("./assets/DarkWave.json")}> </LottieView>
-    </View>
+       <Image style={styles.waves}
+      source={require('../assets/DarkWave.gif')}/>
+       <Image style={styles.logo}
+      source={require('../assets/gurgel.png')}/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container:{
     flex:1,
-    justifyContent:'center',
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#C2DDF0'
   },
-})
+  waves:{
+    flex:1,
+        width:"80%",
+        height:"100%",
+        marginTop:"50%",
+        justifyContent:"center"
+  },
+  logo:{
+    width:"100%",
+    height:"50%"
+  }
+});
